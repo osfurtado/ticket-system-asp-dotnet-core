df71422c5a9a183ea79438075089d717030ee971
OsvaldoFurtado <osvaldofurtado.bitlc@gmail.com>
2026-03-24 15:49:21 +0100

    Working on Tickets

--- TicketSystem.Web/Models/Account/AppUser.cs
+++ TicketSystem.Web/Models/Account/AppUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using TicketSystem.Web.Models.Ticket;
 
 namespace TicketSystem.Web.Models.Account
 {
     public class AppUser: IdentityUser
     {
         public virtual ICollection<AppUserRole> UserRoles { get; set; } = [];
+        public virtual ICollection<TicketModel> Tickets { get; set; } = [];
     }
 }
---
+++ TicketSystem.Web/Models/Ticket/TicketModel.cs
@@ -0,0 +1,29 @@
+﻿using TicketSystem.Web.Models.Account;
+
+namespace TicketSystem.Web.Models.Ticket
+{
+    public class TicketModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+        public int ProjectId { get; set; }
+
+        public required string CreatorId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public required string AssigneeId { get; set; }
+
+        public DateTime? AssignedAt { get; set; }
+
+        public string? ClosedById { get; set; }
+
+        public DateTime? ClosedAt { get; set; }
+
+        public int CurrentStatusId { get; set; }
+
+        public AppUser? CreatedBy { get; set; }
+    }
+}
